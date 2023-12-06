11e9187adb14f218dd230abc6a8be4502f1e033b
Wills Chew <chewwl-wm19@student.tarc.edu.my>
2023-12-06 09:55:22 +0800

    I make some changes for let my apk app screen to sensor portrait

--- app/src/main/java/com/roes/mysqlroomdb/MainActivity.kt
+++ app/src/main/java/com/roes/mysqlroomdb/MainActivity.kt
@@ -4,10 +4,7 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.databinding.DataBindingUtil
 import androidx.recyclerview.widget.RecyclerView
-import com.roes.mysqlroomdb.data.Product
-import com.roes.mysqlroomdb.data.ProductAdapter
-import com.roes.mysqlroomdb.data.ProductDB
-import com.roes.mysqlroomdb.data.ProductDao
+import com.roes.mysqlroomdb.data.*
 import com.roes.mysqlroomdb.databinding.ActivityMainBinding
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers.IO
@@ -28,14 +25,6 @@ class MainActivity : AppCompatActivity() {
 
         dao = ProductDB.getInstance(application).productDao
 
-        val products:List<Product> = listOf(
-            Product(1,"abc",123.00),
-            Product(2,"def",456.00)
-        )
-
-        val myAdapter = ProductAdapter(products)
-        binding.rvView.adapter = myAdapter
-        binding.rvView.setHasFixedSize(true)
 
         //val price: Double = binding.tbPrice.text.toString().toDouble()
         //val btn : Button = findViewById(R.id.btnInsert)
@@ -60,7 +49,6 @@ class MainActivity : AppCompatActivity() {
                 for (p: Product in prodList){
                     productName += p.name + "\n"
                 }
-
 
 
                 CoroutineScope(Main).launch {
